6c83cb81172e5c29505a28917a68928f9ca74c6c
Roman <roman.horbachov.04@gmail.com>
2026-08-17 16:56:41 +0300

    Поднята версия кэша до v26

--- js/app.js
+++ js/app.js
@@ -608,7 +608,7 @@ function renderSettings() {
       <p class="hint">Все данные хранятся только в этом браузере на вашем устройстве и никуда не отправляются. Делайте копию время от времени.</p>
     </div>
 
-    <p class="hint" style="text-align:center" data-action="diag-toggle">Трекер трат · версия 25</p>
+    <p class="hint" style="text-align:center" data-action="diag-toggle">Трекер трат · версия 26</p>
     ${ui.showDiag ? `
     <div class="card">
       <h3>Диагностика экрана</h3>
